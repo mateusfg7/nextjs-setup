68eb8ff79cdbc483a938ad0be97704b974532f84
mateusfg7 <mateusfelipefg77@gmail.com>
2020-12-05 14:58:04 -0300

    convert JS project to TS project

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>Hello Word!</h1>
+      </main>
+    </div>
+  );
+}
